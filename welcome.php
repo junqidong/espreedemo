423ad294be6166cd368f1fbec49ae7262928063f
Chandler Newman-Reed <newm0060@algonquinlive.com>
2016-11-23 18:55:38 -0500

    Added spacing for example ads.

--- welcome.php
+++ welcome.php
@@ -1,14 +1,30 @@
-<?php include_once "common/header.php" ?>
-<?php include_once "common/navbar.php" ?>
-
 <?php
 	session_start();
 	if(!$_SESSION["email"]){
 		header("Location: index.php");
 	}
 ?>
+
+<?php include_once "common/header.php" ?>
+<?php include_once "common/navbar.php" ?>
+
 <div class="container">
 	<div class="row">
+		<div class="col-sm-4">
+		<a class="button-advert" href="#"> Example Ad</a>
+		</div>
+		<div class="col-sm-4">
+		<a class="button-advert" href="#"> Example Ad</a>
+		</div>
+		<div class="col-sm-4">
+		<a class="button-advert" href="#"> Example Ad</a>
+		</div>
+		<div class="col-sm-4">
+		<a class="button-advert" href="#"> Example Ad</a>
+		</div>
+		<div class="col-sm-4">
+		<a class="button-advert" href="#"> Example Ad</a>
+		</div>
 		<div class="col-sm-4">
 		<a class="button-advert" href="#"> Example Ad</a>
 		</div>
